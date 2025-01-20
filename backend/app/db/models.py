93fa731f4a8e0aaedca4126a2c38db559458ff1f
ThomasSommerauer <thomas.sommerauer@speed.at>
2025-01-20 14:25:31 +0100

    models

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -13,7 +13,7 @@ class Role(SQLModel, table=True):
 
     users_at_project: List["UserAtProject"] = Relationship(back_populates="role")
 
-class User1(SQLModel, table=True):
+class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     email: EmailStr = Field(unique=True, index=True, max_length=320)
     display_name: str = Field(max_length=100, nullable=False)
@@ -24,7 +24,7 @@ class User1(SQLModel, table=True):
     is_email_verified: bool = Field(default=False)
     mobile_number: Optional[str] = Field(default=None, max_length=20)
 
-    user_at_projects: List["UserAtProject"] = Relationship(back_populates="user1")
+    user_at_projects: List["UserAtProject"] = Relationship(back_populates="user")
     issues_created: List["Issue"] = Relationship(back_populates="creator")
     issues_responsible: List["Issue"] = Relationship(back_populates="responsible_user")
 
@@ -41,11 +41,11 @@ class Project(SQLModel, table=True):
     user_at_projects: List["UserAtProject"] = Relationship(back_populates="project")
 
 class UserAtProject(SQLModel, table=True):
-    user_id: int = Field(foreign_key="user1.id", primary_key=True)
+    user_id: int = Field(foreign_key="user.id", primary_key=True)
     project_id: int = Field(foreign_key="project.id", primary_key=True)
     role_id: int = Field(foreign_key="role.id")
 
-    user1: "User1" = Relationship(back_populates="user_at_projects")
+    user: "User" = Relationship(back_populates="user_at_projects")
     project: "Project" = Relationship(back_populates="user_at_projects")
     role: "Role" = Relationship(back_populates="users_at_project")
 
@@ -90,34 +90,33 @@ class Issue(SQLModel, table=True):
     category_id: Optional[int] = Field(foreign_key="category.id")
     sprint_id: Optional[int] = Field(foreign_key="sprint.id")
     state_id: Optional[int] = Field(foreign_key="state.id")
-    creator_id: Optional[int] = Field(foreign_key="user1.id")
-    responsible_user_id: Optional[int] = Field(foreign_key="user1.id")
+    creator_id: Optional[int] = Field(foreign_key="user.id")
+    responsible_user_id: Optional[int] = Field(foreign_key="user.id")
     priority_id: Optional[int] = Field(foreign_key="priority.id")
     description: Optional[str] = Field(default=None)
     repository_link: Optional[str] = Field(default=None)
     story_points: Optional[int] = Field(default=None)
     report_time: Optional[str] = Field(default=None)
     version: Optional[int] = Field(default=None)
-    updater_id: Optional[int] = Field(foreign_key="user1.id")
+    updater_id: Optional[int] = Field(foreign_key="user.id")
     project_id: int = Field(foreign_key="project.id")
     updated_at: Optional[str] = Field(default=None)
     created_at: Optional[str] = Field(default=None)
     backlog_order_number: Optional[int] = Field(default=None)
     deleted_at: Optional[str] = Field(default=None)
-    finisher_id: Optional[int] = Field(foreign_key="user1.id")
+    finisher_id: Optional[int] = Field(foreign_key="user.id")
     parent_issue_id: Optional[int] = Field(foreign_key="issue.id")
 
     category: "Category" = Relationship(back_populates="issues")
     sprint: "Sprint" = Relationship(back_populates="issues")
     state: "State" = Relationship(back_populates="issues")
-    creator: "User1" = Relationship(back_populates="issues_created")
-    responsible_user: "User1" = Relationship(back_populates="issues_responsible")
+    creator: "User" = Relationship(back_populates="issues_created")
+    responsible_user: "User" = Relationship(back_populates="issues_responsible")
     priority: "Priority" = Relationship(back_populates="issues")
     project: "Project" = Relationship(back_populates="issues")
     attachments: List["Attachment"] = Relationship(back_populates="issue")
 
-
 ##########################################################
 # export PYTHONPATH=$PWD
-# alembic upgrade head, 
-# alembic revision --autogenerate -m "Add mit neue tabelle6", 
+# alembic upgrade head
+# alembic revision --autogenerate -m "Add mit neue tabelle6"
