a2f241ba97b85ca78d2816186431910fe0a12ad9
Paul Passath <paul.passath@gmail.com>
2025-02-03 15:02:15 +0100

    removed admin check since this already happens in the api

--- backend/app/crud/project.py
+++ backend/app/crud/project.py
@@ -4,15 +4,8 @@ from app.api.deps import get_db
 from app.db.models import User, Project
 from datetime import datetime
 
-def unarchive_project(user_id, project_id):
+def unarchive_project(project_id):
     with next(get_db()) as db:
-        # Get the user by id and check if it is an admin
-        user = db.query(User).filter(User.id == user_id, User.is_admin == True).first()
-
-        # If no user is found or user is not an admin
-        if not user:
-            return None # Or raise custom exception
-        
         # Get project based on the id
         project = db.query(Project).filter(Project.id == project_id).first()
 
