fff82f6e0aa15e5983fa3d98a66c4522d049b68c
zoezp <zoe.zornpauli@gmail.com>
2025-02-03 17:30:25 +0100

    created crud command

---
+++ backend/app/crud/issue.py
@@ -0,0 +1,20 @@
+from typing import Any
+from sqlmodel import Session, select
+from app.api.deps import get_db
+from app.db.models import Issue
+
+# Updates the story points of an issue, returns None if the issue to be changed doesn't exist in the database
+def update_story_point(db: Session, issue_id:int, updated_story_point:int):
+    try:
+        issue = db.query(Issue).filter(Issue.id == issue_id).first()
+
+        issue.story_points = updated_story_point
+
+        db.commit()
+        db.refresh(issue)
+
+        return issue
+
+    except Exception as e:
+        db.rollback()
+        return {"error": f"An error occurred: {str(e)}"}, 500   
