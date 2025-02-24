b518c1259b624048c795d1e7b260a8f5807daa59
MathiasDrei <71633048+MathiasDrei@users.noreply.github.com>
2025-02-24 16:28:30 +0100

    priority_update

--- backend/app/crud/priority.py
+++ backend/app/crud/priority.py
@@ -1,24 +1,48 @@
-from app.db.models import Priority
+from app.db.models import Priority, Issue, User
 from sqlmodel import Session
 
 
-def update_priority(session: Session, issue_id: int, new_priority: str):
+def update_priority(session: Session, issue_id: int, user_id: int, new_priority: str):
     
-    statement = select(Priority).where(Priority.name == new_priority)
-    priority = session.exec(statement).first()
+    user = session.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise ValueError(f"User mit ID {user_id} existiert nicht.")
 
-    if not priority:
-        raise ValueError(f"Priority '{new_priority}' nicht gefunden")
-
-    statement = select(Issue).where(Issue.id == issue_id)
-    issue = session.exec(statement).first()
-
+    issue = session.query(Issue).filter(Issue.id == issue_id).first()
     if not issue:
-        raise ValueError(f"Issue mit ID {issue_id} nicht gefunden")
+        raise ValueError(f"Issue mit ID {issue_id} existiert nicht.")
 
-    issue.priority_id = priority.id
-    session.add(issue)
+    priority = session.query(Priority).filter(Priority.name == new_priority).first()
+    if not priority:
+        raise ValueError(f"Priority '{new_priority}' existiert nicht.")
+
+    new_issue = Issue(
+        id=issue.id,
+        id=issue.id,
+        name=issue.name,
+        category_id=issue.category_id,
+        sprint_id=issue.sprint_id,
+        state_id=issue.state_id,
+        creator_id=issue.creator_id,
+        responsible_user_id=issue.responsible_user_id,
+        priority_id=issue.priority_id,
+        description=issue.description,
+        repository_link=issue.repository_link,
+        story_points=issue.story_points,
+        report_time=issue.report_time,
+        version=issue.version,
+        updater_id=issue.updater_id,
+        project_id=issue.project_id,
+        updated_at=issue.updated_at,
+        created_at=issue.created_at,
+        backlog_order_number=issue.backlog_order_number,
+        deleted_at=issue.deleted_at,
+        finisher_id=issue.finisher_id,
+        parent_issue_id=issue.parent_issue_id,
+    )
+
+    session.add(new_issue)
     session.commit()
-    session.refresh(issue)
+    session.refresh(new_issue)
 
-    return issue  # Gibt das aktualisierte Issue zurück
+    return new_issue  
