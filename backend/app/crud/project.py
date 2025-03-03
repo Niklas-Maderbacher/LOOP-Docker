7565d73e5250f34579e0dc8373cde0235fce24df
marco0679a <marcoallmer@gmail.com>
2025-03-03 14:35:39 +0100

    added updated user role in project (crud)

--- backend/app/crud/project.py
+++ backend/app/crud/project.py
@@ -3,6 +3,7 @@ from typing import List
 from sqlmodel import Session
 from app.db.models import Project
 from app.api.schemas.project import ProjectCreate
+from app.db.models import UserAtProject, Role, User
 
 def get_all_projects(db: Session, skip: int = 0, limit: int = 50) -> List[Project]:
     """Returns all projects from the database.
@@ -37,3 +38,36 @@ def create_project(db: Session, project: ProjectCreate) -> Project:
     db.commit()
     db.refresh(db_project)
     return db_project
+
+
+def update_user_role(db: Session, project_id: int, user_id: int, new_role_id: int):
+    """_summary_
+
+    Args:
+        db (Session): Database session
+        project_id (int): Id of the projekt
+        user_id (int): Id of the user
+        new_role_id (int): Id of the new user role
+
+    Returns:
+        useratproject: _description_
+    """
+    if not db.query(Role).filter(Role.id == new_role_id).first():
+        return None
+    
+    if not db.query(User).filter(User.id == user_id).first():
+        return None
+
+    user_at_project = db.query(UserAtProject).filter(
+        UserAtProject.project_id == project_id,
+        UserAtProject.user_id == user_id
+    ).first()
+
+    if not user_at_project:
+        return None
+
+    user_at_project.role_id = new_role_id
+    db.commit()
+    db.refresh(user_at_project)
+
+    return user_at_project
