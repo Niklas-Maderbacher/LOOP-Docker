35e3ce4684468b70a268efcc740730c404c41f54
Paul Passath <paul.passath@gmail.com>
2025-03-10 14:32:18 +0100

    login button

---
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginButton from './LoginButton/LoginButton.jsx'
+import './LoginPage.modules.css'
+
+function LoginPage() {
+
+    return (
+        <div className='content'>
+            <h1>Login Page</h1>
+            <LoginButton></LoginButton>
+        </div>
+    );
+}
+
+export default LoginPage;
