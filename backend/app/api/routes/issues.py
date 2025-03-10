64870ab099879c84b93e6ca7be3293c8c006a0ca
Paul Passath <paul.passath@gmail.com>
2025-03-10 16:28:45 +0100

    create issue api

--- backend/app/api/routes/issues.py
+++ backend/app/api/routes/issues.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, HTTPException, Depends, Response
 from app.crud.issue import update_story_point
 from app.api.schemas.issue import StoryPointUpdate 
 from app.api.deps import SessionDep
+from app.crud.issue import create_issue
+from app.api.schemas.issue import IssueCreate
+from app.db.models import Issue
 
 router = APIRouter(prefix="/issues", tags=["Issues"])
 
@@ -16,3 +19,24 @@ async def update_issue_story_points(session: SessionDep, issue_id: int, update_d
         return Response(status_code=204)
 
     return updated_issue
+
+@router.post("/", response_model=Issue, status_code=201)
+async def create_new_issue(session: SessionDep, issue_data: IssueCreate):
+    """Creates a new issue in the database.
+
+    Args:
+        session (SessionDep): Database session
+        issue_data (IssueCreate): Issue data
+
+    Returns:
+        Issue: Created issue object
+    """
+    if not issue_data.name or not issue_data.description:
+        raise HTTPException(status_code=400, detail="Name and description are required fields.")
+
+    new_issue = create_issue(session, issue_data)
+
+    if not new_issue:
+        raise HTTPException(status_code=400, detail="Failed to create issue.")
+
+    return new_issue
--- backend/app/crud/issue.py
+++ backend/app/crud/issue.py
@@ -1,5 +1,6 @@
 from sqlmodel import Session
 from app.db.models import Issue
+from app.api.schemas.issue import IssueCreate
 
 def update_story_point(db: Session, issue_id: int, updated_story_point: int):
     try:
@@ -18,3 +19,29 @@ def update_story_point(db: Session, issue_id: int, updated_story_point: int):
     except Exception as e:
         db.rollback()
         return {"error": f"An error occurred: {str(e)}"}, 500
+
+def create_issue(db: Session, issue: IssueCreate) -> Issue:
+    """Creates a new issue in the database.
+
+    Args:
+        db (Session): Database session
+        issue (IssueCreate): Issue details
+
+    Returns:
+        Issue: The created issue instance
+    """
+    db_issue = Issue(
+        name=issue.name,
+        category_id=issue.category_id,
+        sprint_id=issue.sprint_id,
+        responsible_user_id=issue.responsible_id,
+        priority_id=issue.priority_id,
+        description=issue.description,
+        story_points=issue.story_points,
+        project_id=issue.project_id
+    )
+
+    db.add(db_issue)
+    db.commit()
+    db.refresh(db_issue)
+    return db_issue
