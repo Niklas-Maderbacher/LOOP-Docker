f6c9927e704827e7d46ccaea026d0cf9dd3deb13
zoezp <zoe.zornpauli@gmail.com>
2025-03-24 16:36:21 +0100

    added crud and api commands for user

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,44 +1,54 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel, Field
 from typing import List
-
+import app.crud.user as crud
 from app.api.routes import FastApiAuthorization
 from app.api.deps import SessionDep
+from sqlalchemy.exc import IntegrityError
+from app.db.models import User
+from app.api.schemas.user import UserCreate
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-
-# legacy
-
-# test model
-class User(BaseModel):
-    display_name: str = Field(..., min_length=1, max_length=50)
-    password: str = Field(..., min_length=8, max_length=50)
-    is_admin: bool = Field(...)
-    microsoft_account: bool | None = None
-    archived: str | None = None
-    last_active: str | None = None
-    is_email_verified: bool | None = None
-    mobile_number: str | None = None
-
-user_list: List[User] = []
-
-# returns all users
 @router.get("/get_all_users")
-async def get_all_users():
-    return {"users": user_list}
+async def get_all_users(session: SessionDep):
+    """api call to get all users
 
-# path call to return user with specified name
-@router.get("/get_user/{display_name}")
-async def get_user(display_name: str):
-    for user in user_list:
-        if user.display_name == display_name:
-            return {"item": user}
-    raise HTTPException(status_code=404, detail="User not found")
+    Returns:
+        List[User]: list of all users 
+    """
+    return crud.get_all_users(session)
 
-# creates a new user with model
-# requires admin account
+@router.get("/get_user/{email}")
+async def get_user(session: SessionDep, email: str):
+    """api call to get a specific user by email
+
+    Raises:
+        HTTPException: 404 if the user doesn't exist
+
+    Returns:
+        User: the user
+    """
+    user = crud.get_user(session, email)
+    if user == None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
 @router.post("/create_user", dependencies=[Depends(FastApiAuthorization.is_admin)])
-async def create_user(user: User):
-    user_list.append(user)
-    return HTTPException(status_code=201, detail="User created")
+async def register_user(session: SessionDep, user: UserCreate):
+    """api call to create a user
+
+    Args:
+        user (UserCreate): the schema for creating a user (the data)
+
+    Raises:
+        HTTPException: 404 if the user with that email already exists
+
+    Returns:
+        User: the created user 
+    """
+    try:
+        created_user = crud.create_user(session, user)
+        return created_user
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail=f"User with email {user.email} already exist.")
--- backend/app/security/security.py
+++ backend/app/security/security.py
@@ -2,9 +2,9 @@
 # from typing import Any
 #
 # import jwt
-# from passlib.context import CryptContext
-#
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 #
 #
 # ALGORITHM = "HS256"
@@ -21,5 +21,5 @@
 #     return pwd_context.verify(plain_password, hashed_password)
 #
 #
-# def get_password_hash(password: str) -> str:
-#     return pwd_context.hash(password)
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
