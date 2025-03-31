9bd9b734a37bcd015151eb48dc23fba3fb7c1430
Leo Tandl <leojotandl@gmail.com>
2025-03-31 16:34:59 +0200

    model fix

--- backend/app/api/schemas/project.py
+++ backend/app/api/schemas/project.py
@@ -4,5 +4,9 @@ from typing import Optional
 
 class ProjectCreate(BaseModel):
     name: str = Field(max_length=100, nullable=False)
+    key: str = Field(max_length=10, nullable=False)
     start_date: Optional[date] = Field(default=None)
-    github_token: Optional[str] = Field(default=None, max_length=70)
+    end_date: Optional[date] = Field(default=None)
+    github_token: Optional[str]
+
+
