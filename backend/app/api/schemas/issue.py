1be969809c278b8f17c681e28ce5509a4a7500e6
MathiasDrei <71633048+MathiasDrei@users.noreply.github.com>
2025-04-07 15:23:41 +0200

    issue_fix3

--- backend/app/api/schemas/issue.py
+++ backend/app/api/schemas/issue.py
@@ -1,10 +1,13 @@
+from sqlmodel import Field
+from typing import Optional
 from pydantic import BaseModel
 
 class StoryPointUpdate(BaseModel):
     new_story_point_value: int
 
 class GetIssue(BaseModel):
+    project_id: int
     name: str
-    priority_id: int
-    story_points: int
-    responsible_user_id: int
+    priority_id: Optional[int] = Field(default=None)
+    story_points: Optional[int] = Field(default=None)
+    responsible_user_id: Optional[int] = Field(default=None)
