d25a58a7620c5bbab8f397a14cf157f942a9405c
marco0679a <marcoallmer@gmail.com>
2025-05-19 16:50:48 +0200

    added backlog list

--- frontend/src/components/Backlog/Backlog.jsx
+++ frontend/src/components/Backlog/Backlog.jsx
@@ -1,49 +1,41 @@
-//Leo Tandl
 import './Backlog.modules.css';
 import React, { useState } from 'react';
 
-
 function Backlog() {
-    // State variables to control opening and closing of modals and to store new issue data
-    const [isSelectITypeOpen, setIsSelectITypeOpen] = useState(false);  // Controls whether the issue type selection modal is open
-    const [isIssueFormOpen, setIsIssueFormOpen] = useState(false);  // Controls whether the issue creation form modal is open
+    const [isSelectITypeOpen, setIsSelectITypeOpen] = useState(false);
+    const [isIssueFormOpen, setIsIssueFormOpen] = useState(false);
 
-    // Initializes state for the new issue
     const [newIssue, setNewIssue] = useState({ 
-        issueType: "", 
         name: "", 
-        sprint_id: "", 
-        responsible_id: "", 
+        responsible_user_id: "", 
         priority_id: "", 
-        description: "", 
         story_points: "" 
     });
 
-    // Function to open the modal for selecting the issue type
+    // Neuer State für die Liste aller Issues
+    const [issues, setIssues] = useState([]);
+
     function handleOpenSelectIType() {
         setIsSelectITypeOpen(true);
     }
 
-    // Function to handle input field changes
     function handleInputChange(event) {
         const { name, value } = event.target;
         
         setNewIssue((prevIssue) => {
             let newValue = value;
-    
-            // Story Points field validation: only allows numbers
+
             if (name === "story_points") {
-                newValue = value.replace(/[^0-9]/g, "");  // Removes non-numeric characters
+                newValue = value.replace(/[^0-9]/g, "");
                 if (newValue !== "" && parseInt(newValue) < 1) {
-                    newValue = "1"; // Sets minimum value to 1
+                    newValue = "1";
                 }
             }
-    
-            return { ...prevIssue, [name]: newValue };  // Returns updated issue state
+
+            return { ...prevIssue, [name]: newValue };
         });
     }
 
-    // Function to close the issue type selection modal and reset the form state
     function handleCloseSelectIType() {
         setIsSelectITypeOpen(false);
         setNewIssue({ 
@@ -51,94 +43,71 @@ function Backlog() {
             name: "", 
             category_id: "", 
             sprint_id: "", 
-            responsible_id: "", 
+            responsible_user_id: "", 
             priority_id: "", 
             description: "", 
             story_points: "" 
         });
     }
 
-    // Function to handle the submission of the issue type selection
     function handleSubmitIssueType() {
         if (!newIssue.issueType) {
-            alert("Choose the type of your issue!"); // Shows an alert if no issue type is selected
+            alert("Choose the type of your issue!");
             return;
         }
         setIsSelectITypeOpen(false);
-        setIsIssueFormOpen(true); // Opens the form to create the new issue
+        setIsIssueFormOpen(true);
     }
 
-    // Function to close the issue creation form and reset the form state
     function handleCloseIssueForm() {
         setIsIssueFormOpen(false);
         setNewIssue({ 
             issueType: "", 
             name: "", 
+            category_id: "", 
             sprint_id: "", 
-            responsible_id: "", 
+            responsible_user_id: "", 
             priority_id: "", 
             description: "", 
             story_points: "" 
         });
     }
 
-    // Function to handle the submission of the issue creation form
-    async function handleSubmitIssueForm() {
+    function handleSubmitIssueForm() {
         if (!newIssue.name.trim() || !newIssue.description.trim()) {
-            alert("Please fill in all required fields: name and description."); // Shows an alert if name or description is missing
+            alert("Please fill in all required fields: name and description.");
             return;
-        }   
-
-        const response = await fetch("http://localhost:8000/api/v1/issues/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: newIssue.name,
-                category: newIssue.issueType,
-                sprint_id: newIssue.sprint_id || null,
-                responsible_id: newIssue.responsible_id || null,
-                priority_id: newIssue.priority_id || null,
-                description: newIssue.description,
-                story_points: newIssue.story_points || null,
-                project_id: 1
-            })
-        });
-        
-        const data = await response.json();
-        console.log(data);
-        console.log(JSON.stringify({
-            name: newIssue.name,
-            category_id: newIssue.issueType,
-            sprint_id: newIssue.sprint_id || null,
-            responsible_id: newIssue.responsible_id || null,
-            priority_id: newIssue.priority_id || null,
-            description: newIssue.description,
-            story_points: newIssue.story_points || null,
-            project_id: null
-        }));
-
-        setIsIssueFormOpen(false);
-        setNewIssue({ 
-            issueType: "", 
-            name: "", 
-            sprint_id: "", 
-            responsible_id: "", 
-            priority_id: "", 
-            description: "", 
-            story_points: "" 
-        });
+        }
     }
 
     return (
         <div className="Backlog">
-            {/* Button to open the issue type selection modal */}
             <button className="add-issue-btn" onClick={handleOpenSelectIType}>
                 Create Issue
             </button>
 
-            {/* Modal for selecting the issue type */}
+            {/* Tabelle mit allen Issues */}
+            <table className="issue-table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Priority</th>
+                        <th>Story Points</th>
+                        <th>Responsible</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issues.map(issue => (
+                        <tr key={issue.id}>
+                            <td>{issue.name}</td>
+                            <td>{issue.priority}</td>
+                            <td>{issue.story_points}</td>
+                            <td>{issue.responsible_user_id}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
             {isSelectITypeOpen && (
                 <div className="issue-modal">
                     <div className="issue-modal-content">
@@ -149,11 +118,6 @@ function Backlog() {
                             value={newIssue.issueType} 
                             onChange={handleInputChange}
                         >
-                            <option value="">Select...</option>
-                            <option value="Bug">Bug</option>
-                            <option value="Epic">Epic</option>
-                            <option value="User Story">Story</option>
-                            <option value="Subtask">Subtask</option>
                         </select>
                         <div className="modal-buttons">
                             <button onClick={handleSubmitIssueType}>Submit</button>
@@ -163,13 +127,11 @@ function Backlog() {
                 </div>
             )}
 
-            {/* Modal for inputting issue details */}
             {isIssueFormOpen && (
                 <div className="issue-modal">
                     <div className="issue-modal-content">
                         <h2>Create new {newIssue.issueType}</h2>
-                        
-                        {/* Input for the issue name */}
+
                         <input 
                             type="text" 
                             name="name" 
@@ -178,23 +140,10 @@ function Backlog() {
                             onChange={handleInputChange} 
                         />
 
-                        {/* Dropdown for selecting the sprint */}
                         <select
                             className='issue-dropdown' 
-                            name="sprint_id" 
-                            value={newIssue.sprint_id} 
-                            onChange={handleInputChange}
-                        >
-                            <option value="">Sprint ↓</option>
-                            <option value="1">Sprint 1</option>
-                            <option value="2">Sprint 2</option>
-                        </select>
-
-                        {/* Dropdown for selecting the responsible person */}
-                        <select
-                            className='issue-dropdown' 
-                            name="responsible_id" 
-                            value={newIssue.responsible_id} 
+                            name="responsible_user_id" 
+                            value={newIssue.responsible_user_id} 
                             onChange={handleInputChange}
                         >
                             <option value="">Responsible ↓</option>
@@ -202,7 +151,6 @@ function Backlog() {
                             <option value="2">Anna</option>
                         </select>
 
-                        {/* Dropdown for selecting the priority */}
                         <select
                             className='issue-dropdown' 
                             name="priority_id" 
@@ -210,40 +158,14 @@ function Backlog() {
                             onChange={handleInputChange}
                         >
                             <option value="">Priority ↓</option>
-                            <option value="Very high">Very high</option>
-                            <option value="High">High</option>
-                            <option value="Medium">Medium</option>
-                            <option value="Low">Low</option>
-                            <option value="Very low">Very low</option>
+                            <option value="1">Low</option>
+                            <option value="2">Medium</option>
+                            <option value="3">High</option>
                         </select>
 
-                        {/* Input for the issue description */}
-                        <input 
-                            type="text"
-                            name="description" 
-                            placeholder="Description" 
-                            value={newIssue.description} 
-                            onChange={handleInputChange} 
-                        />
-
-                        {/* Parent Issue dropdown shown only for Subtasks */}
-                        {newIssue.issueType === "subtask" && (
-                            <select
-                                className='issue-dropdown'
-                                name="parent_issue_id"
-                                value={newIssue.parent_issue_id}
-                                onChange={handleInputChange}
-                            >
-                                <option value="">Parent Issue</option>
-                                <option value="1">Parent Issue 1</option>
-                                <option value="2">Parent Issue 2</option>
-                            </select>
-                        )}
-
-                        {/* Story Points field shown only for Epic or Subtask */}
                         {["story", "subtask"].includes(newIssue.issueType) && (
                             <input 
-                                type="text"  // Text input to prevent the number input spinner
+                                type="text"
                                 name="story_points" 
                                 placeholder="Story Points (optional)" 
                                 value={newIssue.story_points} 
